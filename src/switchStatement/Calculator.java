16a4672019d7200d0501cc323a50e753f579de49
Rajat-Spy <cse.18bcs1876@gmail.com>
2021-04-27 15:45:31 +0530

    Switch

---
+++ src/switchStatement/Calculator.java
@@ -0,0 +1,35 @@
+package switchStatement;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Numbers:  ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.nextLine();
+		char c = sc.nextLine().charAt(0);
+		
+		switch(c) {
+		case '+':
+			System.out.println(a+b);
+			break;
+		case '-':
+			System.out.println(a-b);
+			break;
+		case '*':
+			System.out.println(a*b);
+			break;
+		case '/':
+			System.out.println(a/b);
+			break;
+		default:
+			System.out.println("The operation is wrong !...");
+		}
+		
+
+	}
+
+}
