e99e1d27842eddb219d6a9c7a52d68334b8a2739
Rajat-Spy <cse.18bcs1876@gmail.com>
2021-04-27 15:44:22 +0530

    Loops

---
+++ src/loops/ForLoops.java
@@ -0,0 +1,15 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int res = (n*(n+1))/2;
+		System.out.println(res);
+
+	}
+
+}
